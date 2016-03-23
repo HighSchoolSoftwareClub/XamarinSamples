e03bc0ce5035f1c0a104217a0f4d3e361881ab88
Barış Ceviz <baris_vebsoft@hotmail.com>
2016-03-23 20:14:39 +0200

    HelloWorldXamarinForms

---
+++ HelloWorldXamarinForms/HelloWorld/HelloWorld/Views/LoginPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HelloWorld.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Signup_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignupPage());
+        }
+
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Username.Text) && string.IsNullOrEmpty(Password.Text))
+                await DisplayAlert("LoginBox", "Please check your username and password", "Ok");
+        }
+    }
+}
---
+++ HelloWorldXamarinForms/HelloWorld/HelloWorld/Views/SignupPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HelloWorld.Views
+{
+    public partial class SignupPage : ContentPage
+    {
+        public SignupPage()
+        {
+            InitializeComponent();
+        }
+
+        void Signup_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
